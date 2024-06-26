7db25803e2b76cc8f65edf0098531354daf761dc
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-06-26 23:41:36 +0300

    API

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const apiKeys ="7014728ebc18e1989f4fc892bc7e02a2"
+const accessToken ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDE0NzI4ZWJjMThlMTk4OWY0ZmM4OTJiYzdlMDJhMiIsIm5iZiI6MTcxOTM2MDM2OS44MzM2NjEsInN1YiI6IjY2N2E0ZTIwOTA4MjgyNDYxMDU3M2Y0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PyySv-16ttssB8kVG2Obx3epUuSJNgitU-vSzfTWLdI"
+
+const baseUrl = "https://api.themoviedb.org/3"
+const getMovies="discover/movie"
+
