bd9c89fdcf118aea15fb9061185bf3ca00b5c48b
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-07-23 07:50:37 +0300

    finish hero

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
-const apiKeys ="7014728ebc18e1989f4fc892bc7e02a2"
-const accessToken ="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MDE0NzI4ZWJjMThlMTk4OWY0ZmM4OTJiYzdlMDJhMiIsIm5iZiI6MTcxOTM2MDM2OS44MzM2NjEsInN1YiI6IjY2N2E0ZTIwOTA4MjgyNDYxMDU3M2Y0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PyySv-16ttssB8kVG2Obx3epUuSJNgitU-vSzfTWLdI"
+fetch('https://api.themoviedb.org/3/discover/movie?api_key=7014728ebc18e1989f4fc892bc7e02a2')
+  .then(response => response.json())
+  .then(function(data) {
+    let movies=data.results
 
-const baseUrl = "https://api.themoviedb.org/3"
-const getMovies="discover/movie"
+    movies.map(movie=>{
 
+        let content=`
+         <div class="owl-carousel-info-wrap item">
+                                    <img src="${baseImageUrl}${movie.poster_path}" class="owl-carousel-image img-fluid" alt="">
+                                    <img src="images/${movie.adult ? '18':'verified'}.png" class="owl-carousel-verified-image img-fluid" alt="">
+                                    <div class="owl-carousel-info">
+                                        <h4 class="mb-2">
+                                            ${movie.original_title}
+                                        </h4>
+                                        <span class="badge">Lang ${movie.original_language}</span>
+                                        <span class="badge">Rate: ${movie.vote_average}</span>
+                                    </div>
+        `
+        document.querySelector('.owl-carousel').innerHTML+=content
+    })
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        autoplay: true,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 2,
+            },
+            767:{
+                items: 3,
+            },
+            1200:{
+                items: 4,
+            }
+        }
+    });
+})
